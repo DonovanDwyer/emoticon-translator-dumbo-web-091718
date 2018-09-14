066023b3647b90751172cebec0dbf9d7fe19a085
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-14 15:56:35 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require "pry"
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   get_meaning =  emoticons.each_with_object({}) {|a,h| h[a[1][1]] = a[0]}
-  get_emoticon = emoticons.each_with_object({}) {|a,h| h[a[1][0] = a[1][1]}
+  get_emoticon = emoticons.each_with_object({}) {|a,h| h[a[1][0]] = a[1][1]}
   binding.pry
   emo_hash
 end
