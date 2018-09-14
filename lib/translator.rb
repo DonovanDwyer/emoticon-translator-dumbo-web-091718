cf6cd5f6b5f95324e89787a0f29361f10fd0234a
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-14 15:58:51 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,6 +7,7 @@ def load_library(file_path)
   get_meaning =  emoticons.each_with_object({}) {|a,h| h[a[1][1]] = a[0]}
   get_emoticon = emoticons.each_with_object({}) {|a,h| h[a[1][0]] = a[1][1]}
   emo_hash["get_meaning"] = get_meaning
+  emo_hash["get_emoticon"] = get_emoticon
   binding.pry
   emo_hash
 end
