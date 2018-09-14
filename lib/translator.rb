571d05b53cf92e712fa6489e68bedeee3fc0ad1d
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-14 15:39:14 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,6 +3,10 @@ require "pry"
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
+  emo_hash = {}
+  emoticons.each do |k,v|
+    new_hash[k] = v[0]
+  end
   binding.pry
 end
 
